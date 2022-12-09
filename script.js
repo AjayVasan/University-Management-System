9b35544e92531bf897789507121b1089f8eaa643
AjayVasan <kowshikvasan9090@gmail.com>
2022-12-09 20:34:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function cop(v){
+    var c=document.getElementById(v).value;
+    document.getElementById("g").innerHTML= 'Student Name : '+c;
+}
+function up(b){
+    var x=document.getElementById(b);
+    x.value=x.values.charAt(0).toUpperCase();
+}
+function login(){
+    var g=document.getElementById("nid").value;
+    var k=document.getElementById("rid").value;
+
+}
